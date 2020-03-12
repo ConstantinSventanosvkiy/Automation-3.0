4d2fc6eb83366b336b0056b05a72d367ab6bde5a
Constantin.Sventanovskiy <Constantin.Sventanovskiy@idt.net>
2020-03-12 11:14:01 +0300

    Lesson 5 homework

---
+++ src/main/java/MyHomeTaskLesson5/MyHomeTaskLesson5.java
@@ -0,0 +1,32 @@
+package MyHomeTaskLesson5;
+
+import java.util.Scanner;
+
+public class MyHomeTaskLesson5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int numberFirst = scanner.nextInt();
+        System.out.print("Type another number: ");
+        int numberSecond = scanner.nextInt();
+        outputResultToConsole(numberFirst, numberSecond);
+    }
+
+    public static void outputResultToConsole(int numberFirst, int numberSecond) {
+        System.out.println("вы сложили число " + numberFirst + " и число " + numberSecond + ", сумма равна " +
+                plus(numberFirst, numberSecond)
+        );
+        System.out.println("вы вычли из числа " + numberFirst + " число " + numberSecond + ", разница равна " +
+                minus(numberFirst, numberSecond)
+        );
+    }
+
+    public static int plus(int numberFirst, int numberSecond) {
+        return numberFirst + numberSecond;
+    }
+
+    public static int minus(int numberFirst, int numberSecond) {
+        return numberFirst - numberSecond;
+    }
+}
